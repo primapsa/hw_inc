3a2a5fb1c7305dfeba2136244794e5b4c7b00887
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-04 15:38:12 +0300

    fix: correct minutes

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -41,7 +41,7 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = `${formattInput(date.getHours())}:${formattInput(date.getHours())}:${formattInput(date.getSeconds())}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = `${formattInput(date.getHours())}:${formattInput(date.getMinutes())}:${formattInput(date.getSeconds())}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = `${formattInput(date.getDate())}.${formattMonth(date.getMonth())}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
     const [dayName, monthName] = formatter.format(date).split(' ')
 
