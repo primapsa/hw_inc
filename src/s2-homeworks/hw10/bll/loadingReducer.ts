2148acff996f7dd5b6e1e41b85f9f50bab275df3
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-23 22:30:39 +0300

    feat : hw 10 reducer

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,15 @@
+import {AppStoreType} from "./store";
+export type LoadingStateType = {
+    isLoading: boolean
+}
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: LoadingStateType = initState, action: LoadingActionType): LoadingStateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
+        case "CHANGE_LOADING":
+            return {...state, isLoading: action.isLoading}
 
         default:
             return state
