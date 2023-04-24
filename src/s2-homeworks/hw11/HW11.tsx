85638d1972619d66351f44bc5de46695cb932955
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-24 18:34:56 +0300

    feat : add change fn

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -17,6 +17,13 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+
+        if (Array.isArray(value)) {
+            let [v1, v2] = value
+            setValue1(v1)
+            setValue2(v2)
+        } else
+            setValue1(value)
     }
 
     return (
@@ -30,6 +37,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
+                            onChange={change}
+                            value={value1}
 
                         />
                     </div>
@@ -38,6 +47,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
+                            onChange={change}
+                            value={[value1, value2]}
 
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
