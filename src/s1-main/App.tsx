90a3f9d6f946fd1427397e08212549056a526ba4
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-24 17:41:03 +0300

    init : start hw 11

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import s from './App.module.css'
 import HW5 from "../s2-homeworks/hw05/HW5";
-import HW6 from "../s2-homeworks/hw06/HW6";
-import HW7 from "../s2-homeworks/hw07/HW7";
-import HW8 from "../s2-homeworks/hw08/HW8";
-import HW9 from "../s2-homeworks/hw09/HW9";
-import HW10 from "../s2-homeworks/hw10/HW10";
 
 function App() {
     return (
         <div className={s.App}>
             <HW5/>
-            <HW6/>
-            <HW7/>
-            <HW8/>
-            <HW9/>
-            <HW10/>
         </div>
     )
 }
