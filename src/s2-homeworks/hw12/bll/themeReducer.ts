c412b6ad4b4b4e4f38b044136851bd598ff008ab
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-26 22:13:13 +0300

    feat : add theme reducer

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,18 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (state: StateType = initState, action: ActionType): StateType => { // fix any
 
+    switch (action.type) {
+        case "SET_THEME_ID":
+            return {themeId: action.id}
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export type StateType = {
+    themeId: number
+}
+export type ActionType = changeThemeIdType
+export const changeThemeId = (id: number) => ({type: 'SET_THEME_ID', id} as const) // fix any
+export type changeThemeIdType = ReturnType<typeof changeThemeId>
